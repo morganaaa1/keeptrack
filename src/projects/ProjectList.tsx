2b0f6a5f15bf04efd49fb2a2fbcfcf8bde460656
ali <alinabilahramadhan@gmail.com>
2023-01-16 13:02:01 +0700

    create a form components

--- src/projects/ProjectList.tsx
+++ src/projects/ProjectList.tsx
@@ -1,24 +1,21 @@
-import React from 'react'
-import { Project } from './Project'
-import PropTypes from 'prop-types';
+import React from 'react';
+import { Project } from './Project';
 import ProjectCard from './ProjectCard';
+import ProjectForm from './ProjectForm';
 
-function ProjectList({ projects }) {
+function ProjectList ({ projects }) {
     const items = projects.map(project => (
         <div key={project.id} className="cols-sm">
             <ProjectCard project={project}></ProjectCard>
+            <ProjectForm/>
         </div>
     ));
 
-    return (
+    return(
         <div className='row'>
             {items}
         </div>
     )
-}
-
-ProjectList.propTypes = {
-    projects: PropTypes.arrayOf(PropTypes.instanceOf(Project)).isRequired
 }
 
 export default ProjectList
