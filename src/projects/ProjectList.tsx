081265628768a9c56e17fc5b0bc390bd0e2ff157
ali <alinabilahramadhan@gmail.com>
2023-01-16 13:15:23 +0700

    hide and show compononents

--- src/projects/ProjectList.tsx
+++ src/projects/ProjectList.tsx
@@ -1,21 +1,25 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { Project } from './Project';
 import ProjectCard from './ProjectCard';
 import ProjectForm from './ProjectForm';
 
-function ProjectList ({ projects }) {
-    const items = projects.map(project => (
-        <div key={project.id} className="cols-sm">
-            <ProjectCard project={project}></ProjectCard>
-            <ProjectForm/>
-        </div>
-    ));
+function ProjectList ({ projects }: ProjectListProps) {
+        const [projectBeingEdited, setProjectEdited] = useState({});
 
-    return(
-        <div className='row'>
-            {items}
-        </div>
-    )
-}
+        const handleEdit = (project: Project) => {
+            setProjectEdited(project);
+       };
+    
+        const items = projects.map(project => (
+           <div key={project.id} className="cols-sm">
+            {project === projectBeingEdited ? (
+                <ProjectForm/>
+            ) : (
+                <ProjectCard project={project} onEdit={handleEdit} />
+             )}
+           </div>
+         ));
+         return <div className="row">{items}</div>;
+     }
 
 export default ProjectList
