d3efc7f830df37a0129eb2e2fec971c9e3d0b943
ali <alinabilahramadhan@gmail.com>
2023-01-16 11:22:45 +0700

    Displaying list data

--- src/projects/ProjectsPage.tsx
+++ src/projects/ProjectsPage.tsx
@@ -4,7 +4,6 @@ import ProjectList from './ProjectList'
 
 function ProjectsPage() {
   return (
-    //Use JSON.stringify() to output the MOCK_PROJECTS array from MockProjects.ts in the component.
     <>
     <h1>Projects</h1>
     <ProjectList projects={MOCK_PROJECTS}/>
